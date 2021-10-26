099d355012ad6a94fba51cf1c58f2a39e303d16a
Elzero Web School <ElzeroWebSchool@users.noreply.github.com>
2021-10-26 22:51:47 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*
+ ** Countdown Timer
+ ** Video URL: https://www.youtube.com/watch?v=eFsiOTJrrE8
+ */
+
+// The End Of The Year Date
+// 1000 milliseconds = 1 Second
+
+let countDownDate = new Date("Dec 31, 2021 23:59:59").getTime();
+// console.log(countDownDate);
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
