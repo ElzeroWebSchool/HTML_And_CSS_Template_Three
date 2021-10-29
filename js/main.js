97beabb85555cacfd0bfd9ac14fbba3da92bf6d1
Elzero Web School <ElzeroWebSchool@users.noreply.github.com>
2021-10-30 00:00:53 +0200

    Update JS Code
    Added Increase Number Tutorial

--- js/main.js
+++ js/main.js
@@ -38,13 +38,40 @@ let counter = setInterval(() => {
  ** Video URL: https://youtu.be/sbIoIKI9FOc
  */
 
+/*
+ ** Increase Numbers On Scrolling
+ ** Video URL: https://youtu.be/PLsUdgLnzgQ
+ */
+
 let progressSpans = document.querySelectorAll(".the-progress span");
 let section = document.querySelector(".our-skills");
 
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
 window.onscroll = function () {
+  // Skills Animate Width
   if (window.scrollY >= section.offsetTop - 250) {
     progressSpans.forEach((span) => {
       span.style.width = span.dataset.width;
     });
   }
+  // Stats Increase Number
+  if (window.scrollY >= statsSection.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
 };
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
